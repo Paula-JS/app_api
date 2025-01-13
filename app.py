cc562cc900fe52124f02a71f9bce802e1e5ff04c
Paula-JS <paulaajs20@gmail.com>
2025-01-13 17:59:19 +0100

    errores

--- app.py
+++ app.py
@@ -7,63 +7,70 @@ import pickle
 import sklearn
 import psycopg2
 import io
+from io import BytesIO
 import base64
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt 
 
 app = Flask(__name__)
 
 conexion = "postgresql://postgres:titanicapi@104.199.11.65:5432/postgres"
 engine = create_engine(conexion)
 
+with open("titanic_model.pkl", "rb") as f:
+        modelito = pickle.load(f)
 
 def get_ts():
-    import datetime
     timestamp =datetime.datetime.now().isoformat()
     return timestamp[0:19]
 
-@app.route("/", methods=["GET"])
-def formulario():
+
+@app.route('/', methods=['GET'])
+def home():
     return render_template("formulario.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    pclass = int(request.form().get("pclass"))
-    sex = int(request.form().get("sex"))
-    age = int(request.form().get("age"))
+    pclass = int(request.form.get("pclass"))
+    sex = int(request.form.get("sex"))
+    age = int(request.form.get("age"))
     
-    inputs = [pclass, sex, age]
     
-    with open("titanic_model.pkl", "rb") as f:
-        modelito = pickle.load(f)
-    
-    outputs = modelito.predict([inputs])[0]
+    outputs = modelito.predict([[pclass, sex, age]])
     timestamp = get_ts()
+
     
-    logs = pd.DataFrame({"inputs": [inputs],
-              "predictions": [outputs],
-              "timestamp":[timestamp]})
+    logs = pd.DataFrame({"pclass": [pclass],
+                         "sex": [sex],
+                         "age": [age],
+                         "prediccion": [int(outputs[0])],
+                         "timestamp":[timestamp]})
+    
     logs.to_sql("predictions", con=engine, index=False, if_exists="append")
     
-    import matplotlib.pyplot as plt 
-    fig =plt.figure()
-    logs_leidos.predictions.value_counts().plot(kind="bar")
-    plt.title(f"PREDICTIONS UP TO: {logs_leidos.timestamp.max()}")
     
-    buffer = io.BytesIO()
+    read_predicciones = pd.read_sql("SELECT * FROM predictions", con=engine)
+    fig =plt.figure()
+    read_predicciones.prediccion.value_counts().plot(kind="bar")
+    plt.title("Predicciones totales")
+    
+    buffer = BytesIO()
     plt.savefig(buffer, format="png")
     buffer.seek(0)
     plt.close(fig)
     
-    img_base64 = base64.b64decode(buffer.getvalue()).decode("utf-8")
+    img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
     
-    return render_template("resultado.html", prediccion=outputs, grafica=img_base64)
+    return render_template("resultado.html", prediccion=int(outputs[0]), grafica=img_base64)
     
 
 @app.route("/results", methods=["GET"])
 def results():
     logs_leidos = pd.read_sql("""SELECT * FROM predictions""", con=engine)
     return json.loads(logs_leidos.to_json(orient="records"))
-
 
 
 if __name__ == "__main__":
